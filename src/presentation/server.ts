f29ce3fcfd6c2416f3bcfb73125e2dd7518d35e0
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-11 10:47:47 -0500

    feat: cron service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,12 @@
+import { CronService } from "./cron/cron-service";
+
 export class Server {
     public static start() {
         console.log("Server running...!");
+
+       CronService.createJob(
+        '*/5 * * * * *',
+        () => console.log('each 5 seconds'),
+       );
     }
 }
