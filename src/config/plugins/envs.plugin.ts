3a86b01c5a41122bdcc4c0a2e4a51397c4cc8ef8
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-22 16:37:11 -0500

    feat: email service

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -4,6 +4,8 @@ import * as env from 'env-var';
 export const envs = {
     PROD: env.get('PROD').required().asBool(),
     PORT: env.get('PORT').required().asPortNumber(),
+    MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
+    MAILER_EMAIL_TO: env.get('MAILER_EMAIL_TO').required().asEmailString(),
 }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,9 @@
+import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
-import { CronService } from "./cron/cron-service";
+import { CronService } from "./cron/cron.service";
+import { EmailService } from "./email/email.service";
 
 const fileSystemLogRepository = new LogRepositoryImpl(
     new FileSystemDataSource(),
@@ -10,6 +12,16 @@ const fileSystemLogRepository = new LogRepositoryImpl(
 export class Server {
     public static start() {
         console.log("Server running...!");
+
+        const emailService = new EmailService();
+        emailService.sendEmail({
+            to: envs.MAILER_EMAIL_TO,
+            subject: 'System Logs',
+            htmlBody: `
+                <h3>NOC</h3>
+                <p>lorem ipsum dolor rae</p>
+            `,
+        });
 
         CronService.createJob(
             '*/5 * * * * *',
