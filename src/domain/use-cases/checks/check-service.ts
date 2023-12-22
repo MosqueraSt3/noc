9f4d110978c7b23de350ef255e9fb00683440db1
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-22 11:53:02 -0500

    fix: cleaning code

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -5,8 +5,8 @@ interface CheckServiceUseCase {
     execute( url: string ): Promise<boolean>;
 }
 
-type SuccessCallback = () => void;
-type ErrorCallback = ( error: string ) => void;
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = (( error: string ) => void) | undefined;
 
 export class CheckService implements CheckServiceUseCase {
 
@@ -22,14 +22,14 @@ export class CheckService implements CheckServiceUseCase {
             if ( !req.ok ) throw new Error("Error on check service" + url);
             const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
             this.logRepository.saveLog(log);
-            this.successCallback();
+            this.successCallback && this.successCallback();
 
             return true;
         } catch (error) {
             const errorMessage = `${url} is not ok. ${error}`
             const log = new LogEntity(errorMessage, LogSeverityLevel.high);
             this.logRepository.saveLog(log);
-            this.errorCallback(errorMessage);
+            this.errorCallback && this.errorCallback(errorMessage);
 
             return false;
         }
