f834f1c48a56019fae587cbc7224a706d19a5c2c
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-23 14:47:00 -0500

    feat: changing datasource

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -2,12 +2,14 @@ import { envs } from "../config/plugins/envs.plugin";
 import { CheckService } from "../domain/use-cases/checks/check-service";
 import { SendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+import { MongoLogDataSource } from "../infrastructure/datasources/mongo-log.datasource";
 import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron.service";
 import { EmailService } from "./email/email.service";
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-    new FileSystemDataSource(),
+const logRepository = new LogRepositoryImpl(
+    // new FileSystemDataSource(),
+    new MongoLogDataSource(),
 );
 
 const emailService = new EmailService();
@@ -16,7 +18,7 @@ export class Server {
     public static start() {
         console.log("Server running...!");
 
-        // new SendEmailLogs(emailService, fileSystemLogRepository)
+        // new SendEmailLogs(emailService, logRepository)
         //     .execute(envs.MAILER_EMAIL_TO);
 
 
@@ -25,7 +27,7 @@ export class Server {
             () => {
                 const url = 'https://google.com';
                 new CheckService(
-                    fileSystemLogRepository,
+                    logRepository,
                     () => console.log(`${url} is ok`),
                     ( error ) => console.log( error )
                 ).execute(url);
