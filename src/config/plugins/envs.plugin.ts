2dcc546f7fa344dd2bc9660cf6197544c9fb4e3f
Stiven Mosquera <mosquerast3.dev@gmail.com>
2023-12-23 11:44:23 -0500

    feat: mongo config and docker

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -8,4 +8,9 @@ export const envs = {
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_SECRET_KEY: env.get('MAILER_SECRET_KEY').required().asString(),
     MAILER_EMAIL_TO: env.get('MAILER_EMAIL_TO').required().asEmailString(),
+    MONGO_PORT: env.get('MONGO_PORT').required().asPortNumber(),
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+    MONGO_DB_USER: env.get('MONGO_DB_USER').required().asString(),
+    MONGO_DB_PASS: env.get('MONGO_DB_PASS').required().asString(),
 }
